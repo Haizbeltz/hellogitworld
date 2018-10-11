c606d03c302626358b6cd5b509a4db4c01b2232e
Gorka <gortiz@certiorem.com>
2018-10-11 05:02:20 -0300

    new commit

--- src/main/java/com/github/App.java
+++ src/main/java/com/github/App.java
@@ -10,8 +10,9 @@ public class App
     public static void main( String[] args )
     {
     	//Comment
-        System.out.println( "Hello World!" );
+        System.out.println( "Hola Mundo!" );
     }
 }
 
 //foo
+//xxx
